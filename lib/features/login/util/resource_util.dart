809dbf9bd6056e2da6807cc55515e2e10ca7600e
Lucas da Silva Beraldo <lucas.beraldo@MMB033780CPS.local>
2022-04-04 15:39:14 -0300

    feat: resource to login page

---
+++ lib/features/login/util/resource_util.dart
@@ -0,0 +1,8 @@
+abstract class ResourceUtil {
+  static String loginTitle = 'Get your Money Under Control';
+  static String loginSubtitle = 'Manage your expenses. Seamslessly';
+  static String signUpButtonEmail = 'Sign Up with Email ID';
+  static String signUpButtonGoogle = 'Sign Up with Google';
+  static String signAlreadyText = 'Already have an account? ';
+  static String signText = 'Sign In';
+}
