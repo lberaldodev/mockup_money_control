253a822d541e74adc064b3defab27c8a7581c580
Lucas da Silva Beraldo <lucas.beraldo@MMB033780CPS.local>
2022-04-04 15:39:33 -0300

    feat: default theme info to textfields and color

---
+++ lib/core/theme/colors_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorsTheme {
+  static Color background = Colors.black;
+  static Color secondary = Colors.white;
+  static Color secondaryVariant = Colors.white60;
+  static Color primary = const Color(0xff6c5ce7);
+  static Color primaryVariant = Colors.black;
+}
---
+++ lib/core/theme/text_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TextThemeUtil {
+  static TextStyle textXSmall = const TextStyle(fontSize: 16);
+  static TextStyle textSmall = const TextStyle(fontSize: 18);
+  static TextStyle textMedium = const TextStyle(fontSize: 23);
+  static TextStyle textLarge = const TextStyle(fontSize: 36);
+  static FontWeight textWeightNormal = FontWeight.w400;
+  static FontWeight textWeightMedium = FontWeight.w500;
+}
